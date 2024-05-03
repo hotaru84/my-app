9bce1e70e0ac2e51788969c9eb31dd96427aefe1
hotaru84 <c11t11w@msn.com>
2024-05-04 00:19:59 +0900

    cal

--- src/CalendarChart.tsx
+++ src/CalendarChart.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef } from "react";
+import { FC, useEffect, useRef } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,9 +9,20 @@ import {
   TimeScale,
   LineElement,
   PointElement,
+  ChartOptions,
+  PluginOptionsByType,
+  ScaleOptionsByType,
+  TimeScaleOptions,
+  ScaleOptions,
+  ScaleChartOptions,
+  ChartData,
+  ChartDatasetPropertiesCustomTypesPerDataset,
+  ChartDatasetProperties,
 } from "chart.js";
 import { MatrixController, MatrixElement } from "chartjs-chart-matrix";
 import { Chart } from "react-chartjs-2";
+import { startOfMonth, endOfMonth, format } from "date-fns";
+import { color } from "chart.js/helpers";
 
 ChartJS.register(
   CategoryScale,
@@ -26,49 +37,137 @@ ChartJS.register(
   TimeScale
 );
 
-export const options = {
+export const options: ChartOptions<"matrix"> = {
   maintainAspectRatio: false,
   responsive: true,
   plugins: {
-    legend: {
-      position: "top" as const,
+    tooltip: {
+      displayColors: false,
+      callbacks: {
+        title() {
+          return "";
+        },
+        label(context) {
+          const v = context.raw as MatrixDataType;
+          return ["d: " + v.d, "v: " + v.v.toFixed(2)];
+        },
+      },
     },
   },
   scales: {
-    y: {
-      type: "time" as const,
-      left: "left" as const,
+    x: {
+      type: "time",
+      position: "top",
+      offset: true,
       time: {
-        unit: "day" as const,
+        unit: "day",
         parser: "i",
         isoWeekday: 1,
         displayFormats: {
           day: "iiiiii",
         },
       },
-    },
-    x: {
-      type: "time" as const,
-      position: "top" as const,
-      time: {
-        unit: "day" as const,
+      reverse: false,
+      ticks: {
+        source: "data",
+        padding: 0,
+        maxRotation: 0,
       },
+      grid: {
+        display: false,
+        tickLength: 0,
+      },
+    },
+    y: {
+      type: "time",
+      position: "left",
+      offset: true,
+      time: {
+        unit: "week",
+        round: "week",
+        isoWeekday: 1,
+        displayFormats: {
+          week: "MMM dd",
+        },
+      },
+      ticks: {
+        maxRotation: 0,
+        autoSkip: true,
+        padding: 1,
+      },
+      grid: {
+        display: false,
+        tickLength: 0,
+      },
+      title: {
+        display: true,
+      },
+    },
+  },
+  layout: {
+    padding: {
+      top: 10,
     },
   },
 };
 
-const CalendarChart: FC = () => {
-  const chartRef = useRef(null);
-  const data = {
-    datasets: [
-      {
-        data: [],
-        borderWidth: 1,
-        hoverBackgroundColor: "yellow",
-        hoverBorderColor: "yellowgreen",
+type MatrixDataType = {
+  x: string;
+  y: string;
+  d: string;
+  v: number;
+};
+function isoDayOfWeek(dt: Date) {
+  let wd = dt.getDay(); // 0..6, from sunday
+  wd = ((wd + 6) % 7) + 1; // 1..7 from monday
+  return "" + wd; // string so it gets parsed
+}
+function generateData(): MatrixDataType[] {
+  const now = new Date();
+  const data: MatrixDataType[] = [];
+  let dt = startOfMonth(now);
+  const end = endOfMonth(dt);
+  while (dt <= end) {
+    const iso = format(dt, "yyyy-MM-dd");
+    data.push({
+      x: isoDayOfWeek(dt),
+      y: iso,
+      d: iso,
+      v: Math.random() * 50,
+    });
+    dt = new Date(dt.setDate(dt.getDate() + 1));
+  }
+
+  return data;
+}
+
+const data: ChartData<"matrix", MatrixDataType[]> = {
+  datasets: [
+    {
+      data: generateData(),
+      backgroundColor(context) {
+        const value = context.raw as MatrixDataType;
+        const alpha = (10 + value.v) / 60;
+
+        return color("teal").alpha(alpha).rgbString();
       },
-    ],
-  };
+      borderColor(context) {
+        const value = context.raw as MatrixDataType;
+        const alpha = (10 + value.v) / 60;
+        return color("teal").alpha(alpha).darken(0.3).rgbString();
+      },
+      borderWidth: 1,
+      hoverBackgroundColor: "yellow",
+      hoverBorderColor: "yellowgreen",
+      width: ({ chart }) =>
+        (chart.chartArea || {}).width / chart.scales.x.ticks.length - 3,
+      height: ({ chart }) =>
+        (chart.chartArea || {}).height / chart.scales.y.ticks.length - 3,
+    },
+  ],
+};
+const CalendarChart: FC = () => {
+  const chartRef = useRef<ChartJS<"matrix", MatrixDataType[]>>(null);
 
   return <Chart type="matrix" ref={chartRef} options={options} data={data} />;
 };
